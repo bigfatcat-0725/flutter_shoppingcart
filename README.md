51354adeb7a7c195b1294ef7592a6da8f0d8434c
bigfatcat <bigfatcat0725@gmail.com>
2022-03-07 09:21:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# flutter_shoppingcart
+
+![ui1](assets/ui1.png)
+![ui2](assets/ui2.png)
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'components/shoppingcart_detail.dart';
+import 'components/shoppingcart_header.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme(),
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildShoppingCartAppBar(),
+      body: Column(
+        children: [
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildShoppingCartAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+        SizedBox(
+          width: 16,
+        ),
+      ],
+      elevation: 0.0,
+    );
+  }
+}
